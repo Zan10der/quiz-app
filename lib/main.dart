d47bb89a15ecba9167123c1b4726ee7b74d71d5f
Zan10der <zvanheerden@live.co.za>
2020-05-20 03:20:56 +0200

    Added buttons for quiz answers

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favourite colour?",
+      "What's your favourite animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Quizzer'),
+          title: Text('Perso Quizzer'),
         ),
-        body: Text('Default text!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
